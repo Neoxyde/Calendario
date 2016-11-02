96a6e87c5f75515d3b36162d05970e8326ac8416
dam2 <dam2@PC308.teide.local>
2016-11-02 11:44:26 +0100

    Page decision done
    TODO:
    	Cookie processing
    	Putting data into main page
    	Retrieving data from main page.

--- src/java/Servlet.java
+++ src/java/Servlet.java
@@ -24,16 +24,17 @@ import javax.servlet.http.HttpSession;
 })
 public class Servlet extends HttpServlet
 {
+
     /**
      * Stores the user's name and serves as a login check.
      */
     private HttpSession session;
-    
+
     /**
      * Stores all the users dates.
      */
     private Cookie cookie;
-    
+
     /**
      * Used to tell if the username has been already registered.
      */
@@ -49,122 +50,174 @@ public class Servlet extends HttpServlet
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-	    throws ServletException, IOException
+            throws ServletException, IOException
     {
-	response.setContentType("text/html;charset=UTF-8");
-	
-	//Try to get a session to decide if the user accessed for the first time.
-	session = request.getSession(false);
-	
-	//If the user has accessed for the first time.
-	if (session == null)
-	{
-	    isRegistered = false;
-	    //Retrieve the session.
-	    session = request.getSession();
-	    //Print the first page.
-	    try (PrintWriter out = response.getWriter())
-	    {
-		printFirstPage(out);
-	    }
-	}
-	//If the user already accessed before.
-	else
-	{
-	    //If the user has not got a username assigned yet.
-	    if (!isRegistered)
-	    {
-		session.setAttribute("username", request.getParameter("name"));
-		response.addCookie(new Cookie(request.getParameter("name"), ""));
-		isRegistered = true;
-	    }
-	    else
-	    {
-		//TODO add data handling from cookie
-	    }
-	    
-	    //Print the main page.
-	    try (PrintWriter out = response.getWriter())
-	    {
-		printMainPage(out,(String) session.getAttribute("username"));
-	    }
-	}
+        response.setContentType("text/html;charset=UTF-8");
+
+        //Try to get a session to decide if the user accessed for the first time.
+        session = request.getSession(false);
+
+        //If the user has accessed for the first time.
+        if (session == null)
+        {
+            isRegistered = false;
+            //Retrieve the session.
+            session = request.getSession();
+            
+            //Print the first page.
+            try (PrintWriter out = response.getWriter())
+            {
+                printFirstPage(out);
+            }
+            
+        } //If the user already accessed before.
+        else
+        {
+            //If the user has not got a username assigned yet.
+            if (!isRegistered)
+            {
+                //Set the session name as the username.
+                session.setAttribute("username", request.getParameter("name"));
+                
+                //Add a cookie with id as the username.
+                response.addCookie(new Cookie(request.getParameter("name"), ""));
+                
+                //Set the user as already registered.
+                isRegistered = true;
+            } else
+            {
+                //TODO add data handling from cookie
+                
+                //Retrieve the cookie.
+                getCookie(request);
+                
+                //If the user has any dates
+                if (!cookie.getValue().isEmpty())
+                {
+                    String[] dates = handleCookie(cookie);
+                }
+                
+                
+            }
+
+            //Print the main page.
+            try (PrintWriter out = response.getWriter())
+            {
+                printMainPage(out, (String) session.getAttribute("username"));
+            }
+        }
     }
-    
+
     /**
      * Prints the first page.
+     *
      * @param out The PrintWriter used to output the page.
      */
     private void printFirstPage(PrintWriter out)
     {
-	out.println("<!DOCTYPE html>\n"
-		+ "<html lang=\"es\">\n"
-		+ "<head>\n"
-		+ "    <meta charset=\"UTF-8\">\n"
-		+ "    <title>Calendario</title>\n"
-		+ "    <!--<link rel=\"stylesheet\" href=\"headerStyle.css\">-->\n"
-		+ "</head>\n"
-		+ "<body>\n"
-		+ "    <div class=\"header\"><h2>Mi Calendario</h2></div>\n"
-		+ "    <div class=\"body\">\n"
-		+ "        <div id=\"msgwelcome\">Introduce tu nombre</div>\n"
-		+ "        <div id=\"nameform\">\n"
-		+ "            <form action=\"Servlet\" method=\"post\">\n"
-		+ "                <input type=\"text\" placeholder=\"Nombre\" name=\"name\">\n"
-		+ "                <input type=\"reset\" value=\"Restablecer\">\n"
-		+ "                <input type=\"submit\" value=\"Enviar\">\n"
-		+ "            </form>\n"
-		+ "        </div>\n"
-		+ "    </div>\n"
-		+ "</body>\n"
-		+ "</html>");	
+        out.println("<!DOCTYPE html>\n"
+                + "<html lang=\"es\">\n"
+                + "<head>\n"
+                + "    <meta charset=\"UTF-8\">\n"
+                + "    <title>Calendario</title>\n"
+                + "    <!--<link rel=\"stylesheet\" href=\"headerStyle.css\">-->\n"
+                + "</head>\n"
+                + "<body>\n"
+                + "    <div class=\"header\"><h2>Mi Calendario</h2></div>\n"
+                + "    <div class=\"body\">\n"
+                + "        <div id=\"msgwelcome\">Introduce tu nombre</div>\n"
+                + "        <div id=\"nameform\">\n"
+                + "            <form action=\"Servlet\" method=\"post\">\n"
+                + "                <input type=\"text\" placeholder=\"Nombre\" name=\"name\">\n"
+                + "                <input type=\"reset\" value=\"Restablecer\">\n"
+                + "                <input type=\"submit\" value=\"Enviar\">\n"
+                + "            </form>\n"
+                + "        </div>\n"
+                + "    </div>\n"
+                + "</body>\n"
+                + "</html>");
     }
-    
+
     /**
      * Prints the main page.
+     *
      * @param out The PrintWriter used to output the page.
-     * @param username String used to customize the main page with the user's name.
+     * @param username String used to customize the main page with the user's
+     * name.
      */
     private void printMainPage(PrintWriter out, String username)
     {
-	out.println("<!DOCTYPE html>\n"
-		+ "<html lang=\"en\">\n"
-		+ "<head>\n"
-		+ "    <meta charset=\"UTF-8\">\n"
-		+ "    <title>Calendario</title>\n"
-		+ "    <!--<link rel=\"stylesheet\" href=\"headerStyle.css\">-->\n"
-		+ "</head>\n"
-		+ "<body>\n"
-		+ "    <div class=\"header\"><h2>Mi Calendario</h2></div>\n"
-		+ "    <div class=\"body\">\n"
-		+ "        <div id=\"msgwelcome\">Bienvenido, " + username +"</div>\n"
-		+ "        <div class=\"dates\" style=\"margin-bottom:2em;\">\n"
-		+ "            <h4>Citas</h4>\n"
-		+ "            <p>Día Mes Año Descripción</p>\n"
-		+ "        </div>\n"
-		+ "        <div class=\"newdate\" style=\"margin-top:2em;\">\n"
-		+ "            <h4>Crear nueva cita</h4>\n"
-		+ "            <form action=\"Servlet\" method=\"POST\">\n"
-		+ "                Fecha\n"
-		+ "                <br>\n"
-		+ "                <input type=\"date\" name=\"date\" id=\"date\">\n"
-		+ "                <br><br>\n"
-		+ "                Descripción\n"
-		+ "                <br>\n"
-		+ "                <input type=\"text\" name=\"description\">\n"
-		+ "                <input type=\"submit\" value=\"Enviar\">\n"
-		+ "            </form>\n"
-		+ "        </div>\n"
-		+ "    </div>\n"
-		+ "</body>\n"
-		+ "</html>");
+        out.println("<!DOCTYPE html>\n"
+                + "<html lang=\"en\">\n"
+                + "<head>\n"
+                + "    <meta charset=\"UTF-8\">\n"
+                + "    <title>Calendario</title>\n"
+                + "    <!--<link rel=\"stylesheet\" href=\"headerStyle.css\">-->\n"
+                + "</head>\n"
+                + "<body>\n"
+                + "    <div class=\"header\"><h2>Mi Calendario</h2></div>\n"
+                + "    <div class=\"body\">\n"
+                + "        <div id=\"msgwelcome\">Bienvenido, " + username + "</div>\n"
+                + "        <div class=\"dates\" style=\"margin-bottom:2em;\">\n"
+                + "            <h4>Citas</h4>\n"
+                + "            <p>Día Mes Año Descripción</p>\n"
+                + "        </div>\n"
+                + "        <div class=\"newdate\" style=\"margin-top:2em;\">\n"
+                + "            <h4>Crear nueva cita</h4>\n"
+                + "            <form action=\"Servlet\" method=\"POST\">\n"
+                + "                Fecha\n"
+                + "                <br>\n"
+                + "                <input type=\"date\" name=\"date\" id=\"date\">\n"
+                + "                <br><br>\n"
+                + "                Descripción\n"
+                + "                <br>\n"
+                + "                <input type=\"text\" name=\"description\">\n"
+                + "                <input type=\"submit\" value=\"Enviar\">\n"
+                + "            </form>\n"
+                + "        </div>\n"
+                + "    </div>\n"
+                + "</body>\n"
+                + "</html>");
     }
-    
+
     private void getCookie(HttpServletRequest request)
     {
-	request.getCookies();
+        Cookie[] cookieArray = request.getCookies();
+        
+        boolean flag = true;
+        int i = 0;
+        
+        while (flag)
+        {
+            if (cookieArray[i].getName().equals(session.getAttribute("username")))
+            {
+                cookie = cookieArray[i];
+                flag = false;
+            }
+            else
+            {
+                i++;
+                if (i == cookieArray.length)
+                {
+                    flag = false;
+                }
+            }
+        }
     }
     
+    /**
+     * Processes the raw data from the cookie.
+     * @param cookie The cookie to be processed
+     * @return String array with all the dates
+     */
+    private String[] handleCookie(Cookie cookie)
+    {
+        //Retrieve the raw data from the cookie
+        String rawData = cookie.getValue();
+        
+        //Return all the dates
+        return rawData.split("@");
+    }
     
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -178,9 +231,9 @@ public class Servlet extends HttpServlet
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-	    throws ServletException, IOException
+            throws ServletException, IOException
     {
-	processRequest(request, response);
+        processRequest(request, response);
     }
 
     /**
@@ -193,9 +246,9 @@ public class Servlet extends HttpServlet
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-	    throws ServletException, IOException
+            throws ServletException, IOException
     {
-	processRequest(request, response);
+        processRequest(request, response);
     }
 
     /**
@@ -206,7 +259,7 @@ public class Servlet extends HttpServlet
     @Override
     public String getServletInfo()
     {
-	return "Short description";
+        return "Short description";
     }// </editor-fold>
 
 }
